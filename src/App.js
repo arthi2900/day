8df372fb1c8bb0a2541c6c803c55d5bd1d039864
arthi2900 <aaarthi2927@gmail.com>
2022-03-25 13:21:33 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Movie />
     </div>
   );
 }
 
+function Movie() {
+  const movielist=[
+    {
+      name:"RRR",
+      poster:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@heart_202006300400.png,ox-24,oy-617,ow-29:ote-OTYlICAxMWsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00094579-cmlveamemf-portrait.jpg",
+      about:"RRR is a period drama set in India during the 1920s, revolving around the inspiring journey of two of India`s freedom fighters - Alluri Sitarama Raju and Komaram Bheem.",
+         
+    }]
+  return (
+    <div className="movie-list">
+            <img src={movielist.poster} alt={movielist.name}/>
+    <h4>{movielist.name}</h4>
+    <h6>{movielist.about}</h6>
+    </div>
+  );
+}
 export default App;
